96d93ca1075b11363caf198493b051dc7c203c62
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2015-05-21 15:26:30 +0200

    Release 1.6.0

---
+++ README.rst
@@ -0,0 +1,11 @@
+SmoothGallery
+=============
+
+A TYPO3 extension to create slideshow and galleries.
+
+This is a fork of `rgsmoothgallery`_ 1.5.2 to provide TYPO3 6.2+ compatibility.
+
+Read more in ``doc/`` folder.
+
+.. _rgsmoothgallery: https://forge.typo3.org/projects/extension-rgsmoothgallery
+
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,38 +1,34 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "rgsmoothgallery".
- *
- * Auto generated 21-05-2015 14:26
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'SmoothGallery for TYPO3',
-	'description' => 'Slideshow & Gallery. Shows images from directory folders, from records and inside tt_content (Element "Text with images") DAM and tt_news. +Thumbnail-Browser, +Lightbox. Demo: http://www.rggooglemap.com/dev/rgsmoothgallery.html',
+	'description' => 'Slideshow & Gallery. Shows images from directory folders, from records and inside tt_content (Element "Text with images") DAM and tt_news. +Thumbnail-Browser, +Lightbox.',
 	'category' => 'plugin',
-	'version' => '1.5.2',
+	'shy' => 0,
+	'version' => '1.6.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => 'uploads/tx_rgsmoothgallery/',
+	'modify_tables' => '',
 	'clearcacheonload' => 0,
+	'lockType' => '',
 	'author' => 'Georg Ringer (just2b)',
 	'author_email' => 'http://www.just2b.com',
 	'author_company' => 'http://www.just2b.com',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.1.0-7.99.99',
 		),
-		'conflicts' => 
-		array (
+		'conflicts' => array(
 		),
-		'suggests' => 
-		array (
+		'suggests' => array(
 		),
 	),
 );
-
